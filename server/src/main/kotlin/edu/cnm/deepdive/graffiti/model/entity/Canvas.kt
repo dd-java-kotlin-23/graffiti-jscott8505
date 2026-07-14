8a52d1fb5b462c456d52c60fe50a03661547a241
jazz <jscott85@student.cnm.edu>
2026-07-14 16:46:55 -0600

    Added repositories for Canvas and User, enabling data access operations.

--- server/src/main/kotlin/edu/cnm/deepdive/graffiti/model/entity/Canvas.kt
+++ server/src/main/kotlin/edu/cnm/deepdive/graffiti/model/entity/Canvas.kt
@@ -1,19 +1,9 @@
 package edu.cnm.deepdive.graffiti.model.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.OneToMany
-import jakarta.persistence.OrderBy
-import jakarta.persistence.PrePersist
+import jakarta.persistence.*
 import org.hibernate.annotations.CreationTimestamp
 import java.time.Instant
-import java.util.UUID
+import java.util.*
 
 @Entity
 open class Canvas(
--- server/src/main/kotlin/edu/cnm/deepdive/graffiti/model/entity/User.kt
+++ server/src/main/kotlin/edu/cnm/deepdive/graffiti/model/entity/User.kt
@@ -46,6 +46,5 @@ class User(
         externalKey = UUID.randomUUID()
     }
 
-
 }
 
