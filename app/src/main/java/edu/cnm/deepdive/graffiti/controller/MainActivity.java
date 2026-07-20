5e445065336f33adeb2968aaba342a173274fc8c
jazz <jscott85@student.cnm.edu>
2026-07-20 09:30:24 -0600

    Add logging for lifecycle.

--- app/src/main/java/edu/cnm/deepdive/graffiti/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/graffiti/controller/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.cnm.deepdive.graffiti.controller;
 
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,6 +19,8 @@ import edu.cnm.deepdive.graffiti.databinding.ActivityMainBinding;
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
+  private static final String TAG = MainActivity.class.getSimpleName();
+
   private ActivityMainBinding binding;
   private NavController navController;
   private AppBarConfiguration appBarConfig;
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate");
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     EdgeToEdge.enable(this);
     setContentView(binding.getRoot());
@@ -32,4 +38,72 @@ public class MainActivity extends AppCompatActivity {
       return insets;
     });
   }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume");
+  }
+
+  @Override
+  protected void onPostResume() {
+    super.onPostResume();
+    Log.d(TAG, "onPostResume");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause");
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy");
+  }
+
+  @Override
+  protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    Log.d(TAG, "onRestoreInstanceState");
+    super.onRestoreInstanceState(savedInstanceState);
+  }
+
+  @Override
+  protected void onSaveInstanceState(@NonNull Bundle outState) {
+    Log.d(TAG, "onSaveInstanceState");
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    Log.d(TAG, "onCreateOptionsMenu");
+     super.onCreateOptionsMenu(menu);
+     Log.d(TAG, "onCreateOptionsMenu");
+     getMenuInflater().inflate(R.menu.main_options, menu);
+     return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    Log.d(TAG, "onOptionsItemSelected");
+    if (item.getItemId() == R.id.sign_out) {
+      // TODO: 7/20/26 Do the sign out stuff.
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
 }
